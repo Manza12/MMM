f79e4b94cd624c08e729a269386f3b110e882751
Manza12 <46168726+Manza12@users.noreply.github.com>
2023-08-17 10:06:32 +0200

    Add 2 measures

--- scripts/pianorolls/analysis_textures.py
+++ scripts/pianorolls/analysis_textures.py
@@ -1,13 +1,7 @@
 from mmm.pianorolls.music import *
-from mmm.pianorolls.algorithms import redundancy
 from mmm.pianorolls.midi import create_midi
 from mmm.pianorolls.morphology import erosion, dilation
-from mmm.pianorolls.plot import plot_piano_roll, plot_activations_stack
-
-# Parameters
-# mpl.rcParams['figure.max_open_warning'] = 25
-# c_attack = {'attack': 1, 'sustain': 0}
-# c_sustain = {'attack': 0, 'sustain': 1}
+from mmm.pianorolls.plot import plot_piano_roll  # , plot_activations_stack
 
 # Path
 folder = Path('..') / Path('..') / Path('phd') / Path('chapter_5') / Path('textures')
@@ -30,23 +24,43 @@ texture = Texture(
 # Harmonies
 i = Harmony(
     Chord(-12, 0, 12+3, 24, 24+3),
-    Chord(12+3, 12+7, 24, 24+3),
-    Chord(12, 12+3, 24+3, 24+7),
+    Chord(3, 7, 24, 24+3),
+    Chord(0, 3, 24+3, 24+7),
 )
 iv = Harmony(
     Chord(-7),
-    Chord(12+5, 12+8, 24, 24+5),
-    Chord(12, 12+5, 24+5, 24+8),
+    Chord(5, 8, 24, 24+5),
+    Chord(0, 5, 24+5, 24+8),
 )
 i46 = Harmony(
     Chord(-5),
-    Chord(12+3, 12+7, 24, 24+3),
-    Chord(12, 12+3, 24+3, 24+7),
+    Chord(3, 7, 24, 24+3),
+    Chord(0, 3, 24+3, 24+7),
+)
+V24 = Harmony(
+    Chord(-7),
+    Chord(2, 7, 24-1, 24+2),
+    Chord(-1, 2, 24+2, 24+7),
+)
+i6 = Harmony(
+    Chord(-9),
+    Chord(7, 12, 12+7, 24+3),
+    Chord(0, 3, 24+3, 24+7),
+)
+Np = Harmony(
+    Chord(-7),
+    Chord(8, 13, 12+5, 24+1),
+    Chord(5, 8, 12+8, 24+5),
+)
+i46_2 = Harmony(
+    Chord(-5),
+    Chord(7, 12, 12+3, 24),
+    Chord(3, 7, 12+7, 24+3),
 )
 V = Harmony(
-    Chord(-7),
-    Chord(12+2, 12+7, 24-1, 24+2),
-    Chord(12-1, 12+2, 24+2, 24+7),
+    Chord(-5),
+    Chord(5, 11, 12+2, 24-1),
+    Chord(-1, 5, 12+5, 24+2),
 )
 
 # Harmonic textures
@@ -54,17 +68,25 @@ harmonic_textures = PianoRollStack(
     texture * i,
     texture * iv,
     texture * i46,
+    texture * V24,
+    texture * i6,
+    texture * Np,
+    texture * i46_2,
     texture * V,
 )
 
 # Activations
 start = TimePoint('0/4')
-Gs2 = FrequencyPoint(56-12)
+Gs2 = FrequencyPoint(56)
 activations_harmonic_textures = ActivationsStack(
     Activations(TimeFrequency(start, Gs2)),
     Activations(TimeFrequency(start + TimeShift(1, 2), Gs2)),
     Activations(TimeFrequency(start + TimeShift(2, 2), Gs2)),
     Activations(TimeFrequency(start + TimeShift(3, 2), Gs2)),
+    Activations(TimeFrequency(start + TimeShift(4, 2), Gs2)),
+    Activations(TimeFrequency(start + TimeShift(5, 2), Gs2)),
+    Activations(TimeFrequency(start + TimeShift(6, 2), Gs2)),
+    Activations(TimeFrequency(start + TimeShift(7, 2), Gs2)),
 )
 
 # Piano roll
@@ -80,7 +102,7 @@ activations_texture: ActivationsStack = erosion(piano_roll, texture)
 # Figures
 # Piano roll
 plot_piano_roll(piano_roll, time_label='Time (m, b)',
-                x_tick_start=TimePoint(0), x_tick_step=TimeShift('1/2'),
+                x_tick_start=TimePoint(0), x_tick_step=TimeShift('1'),
                 fig_size=(460, 280), tight_frame=False)
 
 file_path = folder / Path('piano_roll.pdf')
@@ -89,7 +111,7 @@ plt.savefig(file_path)
 # Erosion texture
 for j, activations in enumerate(activations_texture):
     plot_piano_roll(activations, time_label='Time (m, b)', tight_frame=False,
-                    x_tick_start=TimePoint(0), x_tick_step=TimeShift('1/2'),
+                    x_tick_start=TimePoint(0), x_tick_step=TimeShift('1'),
                     fig_size=(400, 260), marker_size=10)
     file_path = folder / Path('erosion_texture-%d.pdf' % (j + 1))
     plt.savefig(file_path)
@@ -101,7 +123,7 @@ for j, pair in enumerate(zip(activations_texture, texture)):
     d.change_tatum(piano_roll.tatum, inplace=True)
     d.change_extension(piano_roll.extension)
     plot_piano_roll(d, time_label='Time (m, b)', tight_frame=False,
-                    x_tick_start=TimePoint(0), x_tick_step=TimeShift('1/2'),
+                    x_tick_start=TimePoint(0), x_tick_step=TimeShift('1'),
                     fig_size=(400, 260))
     file_path = folder / Path('dilation_texture-%d.pdf' % (j + 1))
     plt.savefig(file_path)
