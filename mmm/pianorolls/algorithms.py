151f5da7266ff5c2c6ec0136a10b10b412dad341
Manza12 <46168726+Manza12@users.noreply.github.com>
2023-08-20 09:26:22 +0200

    Add formula to compute the size of a derivation graph from its clusters.

--- mmm/pianorolls/algorithms.py
+++ mmm/pianorolls/algorithms.py
@@ -47,3 +47,28 @@ def synchronize_activations_stack(activations_stack: ActivationsStack):
     contraction_indexes = np.all(contraction_frequency, axis=0, keepdims=True)
     activations_stack_array *= contraction_indexes
     return activations_stack_array
+
+
+def compute_size_graph(clusters: List[List], order_derivation: int):
+    k = order_derivation
+    N = len(clusters) - 1
+
+    sizes = []
+    for cluster in clusters:
+        sizes.append(len(cluster))
+
+    size_v = 0
+    for i in range(k, N + 1):
+        prod = 1
+        for m in range(0, k + 1):
+            prod *= sizes[i - m]
+        size_v += prod
+
+    size_e = 0
+    for i in range(k + 1, N + 1):
+        prod = 1
+        for m in range(0, k + 2):
+            prod *= sizes[i - m]
+        size_e += prod
+
+    return size_v, size_e
