38e7e32624dd2b5476f23d166a465b897d3ae5cb
Manza12 <46168726+Manza12@users.noreply.github.com>
2023-08-21 09:48:12 +0200

    Remove unused plot.

--- scripts/pianorolls/find_minimal_activations_full.py
+++ scripts/pianorolls/find_minimal_activations_full.py
@@ -4,7 +4,7 @@ from mmm.pianorolls.algorithms import find_minimal_activations
 from mmm.pianorolls.music import *
 from mmm.pianorolls.midi import create_midi
 from mmm.pianorolls.morphology import erosion, dilation
-from mmm.pianorolls.plot import plot_piano_roll, plot_activations_stack
+from mmm.pianorolls.plot import plot_piano_roll
 from mmm.pianorolls.graphs import ActivationsGraph, DerivedActivationsGraph
 
 # Parameters
@@ -119,23 +119,6 @@ print(graph_synchronized)
 # Create derived graph of order 0
 derived_graph = DerivedActivationsGraph(graph_synchronized)
 print(derived_graph)
-
-# Figures
-if plot:
-    # Erosion texture
-    plot_activations_stack(activations_synchronized, time_label='Time (m, b)',
-                           tight_frame=False,
-                           x_tick_start=TimePoint(0), x_tick_step=TimeShift('1/2'),
-                           fig_size=(400, 260), marker_size=10,
-                           legend=True,
-                           legend_params={
-                               'columnspacing': 0.2,
-                               'labelspacing': 0.,
-                               'handletextpad': 0.1
-                           })
-    file_path = folder / Path('activations_synchronized.pdf')
-    plt.savefig(file_path)
-
 
 # Find minimal activations
 start = time.time()
